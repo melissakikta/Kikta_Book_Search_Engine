22581f0e5ea79e26419ef05794e313273be68534
Mrs. Kikta <melissakikta@gmail.com>
2025-02-12 20:48:07 -0500

    server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,41 @@
 import express from 'express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4'
 import path from 'node:path';
+
+import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
-import routes from './routes/index.js';
 
-const app = express();
 const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+const startApolloServer = async () => {
+  await server.start();
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server));
+  
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (_, res) => {
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+  }
+    
+  db.on('error', console.error.bind(console, "MongoDB connection error:"));
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+};
+
+startApolloServer();
